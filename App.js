345f8a6fc10fa64b951c8bd3921680cc63cc5901
therajusah <workwithrajukumar@gmail.com>
2023-12-22 23:12:11 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I am an h1 tag"), 
+     React.createElement("h1", {}, "I am an h1 tag")]
+ ),
+ React.createElement(
+    "div",
+    { id: "child3" },
+    [React.createElement("h1", {}, "I am an h3 tag"), 
+     React.createElement("h1", {}, "I am an h4 tag")]
+ )
+);
+
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
+// const heading =  React.createElement("h1", {
+//     id: "heading"
+// }, "Hello World from React!");
+//    const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(heading);
